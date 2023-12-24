b067cd63426013e026295b2814e83b0ebf7ff7ed
pulkitupadhyay <pulkitupadhyay9893@gmail.com>
2023-12-24 18:19:23 +0530

    fixed user issues

--- routes/app.js
+++ routes/app.js
@@ -248,10 +248,7 @@ router.post('/fake_login',async(req,res,next)=>{
     slot.save().then((savedSlot) => {
        
         slot_id = savedSlot._id
-        
-        
-       
-    
+
     
     
         res.render('fake_user_login',{slot_id,employee_id,timezones})
@@ -314,6 +311,36 @@ router.post('/user_signUp', async (req,res,next)=>{
     var user_timezone = req.body.Timezone
     var user_password = req.body.userPassword
 
+
+var userexist = await user_scheema.findOne({ email : user_email})
+
+if(userexist){
+
+    try {
+        // var user1 = await user_scheema.findOne({ email: new_user.email });
+       
+    
+        user=userexist
+       
+        var slot_id = req.body.slot_id;
+var employee_id = req.body.employee_id;
+
+
+
+
+// res.cookie('user_email', new_user.email);
+
+
+res.render('final_slot_book' ,{slot_id,employee_id,user}) 
+    
+    } catch (error) {
+        console.error('Error:', error);
+      }
+
+}else{
+
+
+
     let new_user = new user_scheema({
         name:user_name,
         email: user_email,
@@ -321,6 +348,8 @@ router.post('/user_signUp', async (req,res,next)=>{
         timezone:user_timezone
 
     })
+
+
 var user;
     new_user.save().then(async function(dets){
      
@@ -350,7 +379,7 @@ res.render('final_slot_book' ,{slot_id,employee_id,user})
 
     
 
-
+}
 
 })
 
