2e46203fb76648f5d50ec5f84af1d693391535d0
pulkitupadhyay <pulkitupadhyay9893@gmail.com>
2024-04-26 12:53:36 +0530

    changed the email

--- routes/other_functions/sendMail.js
+++ routes/other_functions/sendMail.js
@@ -6,8 +6,10 @@ async function sendMail(to, subject, text, html) {
     var config = {
       service: "gmail",
       auth: {
-        user: "photo.pulkitfourth@gmail.com",
-        pass: "daapxuseglvonuef",
+        // user: "photo.pulkitfourth@gmail.com",
+        // pass: "daapxuseglvonuef",
+        user: 'swaayatt.interviews@gmail.com', // Your Zoho Mail email address
+        pass: 'zncx rxez hjcy hsny'
       },
     };
   
@@ -16,9 +18,9 @@ async function sendMail(to, subject, text, html) {
     async function main1() {
       // send mail with defined transport object
       const info = await transporter.sendMail({
-        from: '"SWAAYATT" <photo.pulkitfourth@gmail.com>', // sender address
+        from: '"SWAAYATT" <swaayatt.interviews@gmail.com>', // sender address
         to: to,
-        cc: "soumya@swaayatt.com", // list of receivers
+        cc: "soumya@swaayatt.com,pulkit.upadhyay@deepeigen.com", // list of receivers
         subject: subject, // Subject line
         text: text, // plain text body
         html: html, // html body
