064103c8809b0f7146540f64e25532d003668eb1
pulkitupadhyay <pulkitupadhyay9893@gmail.com>
2024-08-06 19:31:21 +0530

    made link working for employee

--- routes/hr_works/employee_signup.js
+++ routes/hr_works/employee_signup.js
@@ -10,12 +10,14 @@ const employee_signUp=async (req, res, next) => {
     var employee_email = req.body.employeeEmail;
     var employee_number = req.body.employeeNumber;
     var employee_password = req.body.employeePassword;
+    var link= req.body.link;
   
     let new_employee = new employee_scheema({
       name: employee_name,
       email: employee_email,
       password: employee_password,
       number: employee_number,
+      link:link
     });
   
     new_employee.save().then(function (dets) {
