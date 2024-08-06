00a53301ffd7a876bec967a6bdf6f1fbc31d13f5
Ajha30 <2gi19is001@students.git.edu>
2024-08-06 18:29:52 +0530

    responsive and added some style

--- public/javascripts/hr_dashbord_js/hr_script.js
+++ public/javascripts/hr_dashbord_js/hr_script.js
@@ -688,7 +688,13 @@ background-color: #e6f7ff;
 <div class="main">
 <div data-id="card-cap" class="cap"></div>
 
-<a href="/hr_dashbord" style="text-align:left;" ><svg xmlns="http://www.w3.org/2000/svg" height="16" width="18" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c.2 35.5-28.5 64.3-64 64.3H128.1c-35.3 0-64-28.7-64-64V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24zM352 224a64 64 0 1 0 -128 0 64 64 0 1 0 128 0zm-96 96c-44.2 0-80 35.8-80 80c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16c0-44.2-35.8-80-80-80H256z"/></svg>GO Back TO Meetings</a>
+<a href="/hr_dashbord" style="display: flex; align-items: center; text-decoration: none; color: #007bff;">
+    <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <path d="M15 18l-6-6 6-6"/>
+    </svg>
+    <span style="margin-left: 8px; font-size: 16px; font-weight: 600;">Go Back to Meetings</span>
+</a>
+
 
 
 
